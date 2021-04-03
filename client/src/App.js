22a49e93d891bc7a9365eeaa08087418b0d98040
JaimeSolisS <jaimesolissoto@me.com>
2021-04-03 00:46:55 -0700

    Feat Login Page and light & dark themes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,48 @@
-import "./App.css";
+import React, { useState } from "react";
+import "./App.scss";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Projects from "./components/projects/Projects";
 
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 function App() {
+  const [theme2, setTheme] = useState("dark");
+
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+          primary: {
+            main: "#F21B3F",
+            light: "#CC2936",
+          },
+          secondary: {
+            main: "#70cfff",
+            light: "#008DD5",
+          },
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/projects" component={Projects} />
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/projects" component={Projects} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,87 @@
 import React from "react";
+import { TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import CheckRoundedIcon from "@material-ui/icons/CheckRounded";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    fontSize: 70,
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    width: theme.spacing(10),
+    height: theme.spacing(10),
+    backgroundColor:
+      theme.palette.type === "dark"
+        ? theme.palette.primary.main
+        : theme.palette.primary.light,
+  },
+  textField: {
+    color:
+      theme.palette.type === "dark"
+        ? theme.palette.secondary.main
+        : theme.palette.secondary.light,
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    background:
+      theme.palette.type === "dark"
+        ? theme.palette.secondary.main
+        : theme.palette.secondary.light,
+  },
+}));
 
 const Login = () => {
-  return <h1>Login page</h1>;
+  const classes = useStyles();
+  const onChangeLogin = () => {};
+  return (
+    <div className="container">
+      <div className="container-form">
+        <Avatar className={classes.avatar}>
+          <CheckRoundedIcon className={classes.icon} />
+        </Avatar>
+        <h1>Log in</h1>
+        <form>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            color="secondary"
+            onChange={onChangeLogin}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            color="secondary"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            className={classes.submit}
+          >
+            Log in
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
