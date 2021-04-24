f06d3beb5ba954fb952d235cd4d0ff246905a83f
JaimeSolisS <jaimesolissoto@me.com>
2021-04-23 22:08:32 -0600

    Delete files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.scss";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
