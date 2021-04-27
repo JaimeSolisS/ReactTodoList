5ca9147ed19ce74b46c94438bed13f2ad4df21e6
JaimeSolisS <jaimesolissoto@me.com>
2021-04-26 19:24:39 -0600

    Feat user validation and hash password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,40 @@
 const User = require("../models/User");
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const { restart } = require("nodemon");
+
 exports.createUser = async (req, res) => {
-  // console.log(req.body);
+  //get email and password
+  const { email, password } = req.body;
 
   try {
-    let user;
+    //Check if user is unique
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(403).json({ msg: "User already exists" });
+    }
+
+    //check errors: Missing parameters, weak password
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
     //crete new user
     user = new User(req.body);
+
+    //Hash password
+    const salt = await bcryptjs.genSalt(10);
+
+    user.password = await bcryptjs.hash(password, salt);
 
     //save user
     await user.save();
 
     //Confirm
-    res.send("User created!");
+    res.status(200).json({ msg: "User created!" });
   } catch (error) {
     console.log(error);
     res.status(400).send("Something happened");
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,9 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController')
+const { check } = require('express-validator');
+
 
 //create user api/users
-router.post('/',
+router.post('/', [
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Use a valid email').isEmail(),
+        check('password', 'Please enter a password at least 8 characters long and contain at least one uppercase, one lower case and at least one special character').isLength({ min: 8 }).matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/, )
+    ],
     userController.createUser
 );
 module.exports = router;
