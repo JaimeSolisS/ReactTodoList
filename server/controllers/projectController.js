2cf9d9764d62db66413ca8bd323f8282049005e9
JaimeSolisS <jaimesolissoto@me.com>
2021-04-28 22:48:45 -0600

    feat get projects and validate project name

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,6 +1,13 @@
 const Project = require("../models/Project");
+const { validationResult } = require("express-validator");
 
 exports.createProject = async (req, res) => {
+  //Check for errors
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   try {
     //Create project
     const project = new Project(req.body);
@@ -11,6 +18,17 @@ exports.createProject = async (req, res) => {
     //save project
     project.save();
     res.json(project);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Some error");
+  }
+};
+
+//Get user projects
+exports.getProjects = async (req, res) => {
+  try {
+    const projects = await Project.find({ owner: req.user.id });
+    res.json({ projects });
   } catch (error) {
     console.log(error);
     res.status(500).send("Some error");
--- server/routes/projects.js
+++ server/routes/projects.js
@@ -2,9 +2,17 @@ const express = require("express");
 const router = express.Router();
 const projectController = require("../controllers/projectController");
 const auth = require("../middleware/auth");
+const { check } = require("express-validator");
 
 //Create projects
 //api/projects
-router.post("/", auth, projectController.createProject);
+router.post(
+  "/",
+  auth,
+  [check("name", "Enter a project name").notEmpty()],
+  projectController.createProject
+);
+
+router.get("/", auth, projectController.getProjects);
 
 module.exports = router;
