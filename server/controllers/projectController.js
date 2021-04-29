8ba6e8403d2c50857135d9051514c2cfc75be963
JaimeSolisS <jaimesolissoto@me.com>
2021-04-28 21:52:45 -0600

    feat create project

---
+++ server/controllers/projectController.js
@@ -0,0 +1,13 @@
+const Project = require("../models/Project");
+
+exports.createProject = async (req, res) => {
+  try {
+    //Create project
+    const project = new Project(req.body);
+    project.save();
+    res.json(project);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Some error");
+  }
+};
---
+++ server/routes/projects.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const projectController = require("../controllers/projectController");
+
+//Create projects
+//api/projects
+router.post("/", projectController.createProject);
+
+module.exports = router;
