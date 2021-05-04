072e1be93a52c8a4f98dab6d6101e5dd3797cdf9
JaimeSolisS <jaimesolissoto@me.com>
2021-05-04 01:05:06 -0600

    feat insert tutorial when a new user is registered

--- client/src/context/auth/authState.js
+++ client/src/context/auth/authState.js
@@ -8,6 +8,8 @@ import {
   LOGIN_SUCCESS,
   LOGIN_ERROR,
   SIGN_OUT,
+  ADD_PROJECT,
+  ADD_TASK,
 } from "../../types";
 import axiosClient from "../../config/axios";
 import authToken from "../../config/authToken";
@@ -27,7 +29,6 @@ const AuthState = (props) => {
   const registerUser = async (data) => {
     try {
       const res = await axiosClient.post("/api/users", data);
-      //console.log(res);
 
       dispatch({
         type: REGISTER_SUCCESS,
@@ -35,8 +36,107 @@ const AuthState = (props) => {
       });
 
       //get user
-
       userAuthenticated();
+
+      //Insert tutorial in User account
+      //Project
+      const resP = await axiosClient.post("/api/projects", {
+        name: " Welcome 👋",
+      });
+      dispatch({
+        type: ADD_PROJECT,
+        payload: resP.data,
+      });
+      console.log("id project", resP.data._id);
+      //Tasks
+      //1)
+      const resT1 = await axiosClient.post("/api/tasks", {
+        name: "This task is completed ☑️",
+        status: true,
+        project: resP.data._id,
+        dueDate: null,
+      });
+      dispatch({
+        type: ADD_TASK,
+        payload: resT1.data.task,
+      });
+      //2)
+      const resT2 = await axiosClient.post("/api/tasks", {
+        name: "Click the ⭕️ to mark this task as completed",
+        status: false,
+        project: resP.data._id,
+        dueDate: null,
+      });
+      dispatch({
+        type: ADD_TASK,
+        payload: resT2.data.task,
+      });
+      //3)
+      const resT3 = await axiosClient.post("/api/tasks", {
+        name: "Add a new task ➕ with the button below",
+        status: false,
+        project: resP.data._id,
+        dueDate: null,
+      });
+      dispatch({
+        type: ADD_TASK,
+        payload: resT3.data.task,
+      });
+      //4)
+      const resT4 = await axiosClient.post("/api/tasks", {
+        name: "Edit this task's name ✏️ ",
+        status: false,
+        project: resP.data._id,
+        dueDate: null,
+      });
+      dispatch({
+        type: ADD_TASK,
+        payload: resT4.data.task,
+      });
+      //5)
+      const resT5 = await axiosClient.post("/api/tasks", {
+        name: "Schedule this task 📅",
+        status: false,
+        project: resP.data._id,
+        dueDate: null,
+      });
+      dispatch({
+        type: ADD_TASK,
+        payload: resT5.data.task,
+      });
+      //6)
+      const resT6 = await axiosClient.post("/api/tasks", {
+        name: "Delete this task 🗑",
+        status: false,
+        project: resP.data._id,
+        dueDate: null,
+      });
+      dispatch({
+        type: ADD_TASK,
+        payload: resT6.data.task,
+      });
+      //7)
+      const resT7 = await axiosClient.post("/api/tasks", {
+        name: "Click ➕ next to Projects to add one of your own",
+        status: false,
+        project: resP.data._id,
+        dueDate: null,
+      });
+      dispatch({
+        type: ADD_TASK,
+        payload: resT7.data.task,
+      });
+      //8)
+      const resT8 = await axiosClient.post("/api/tasks", {
+        name: "Click 🗑 below to delete this project",
+        status: false,
+        project: resP.data._id,
+        dueDate: null,
+      });
+      dispatch({
+        type: ADD_TASK,
+        payload: resT8.data.task,
+      });
     } catch (error) {
       //console.log(error.response.data.msg);
       const alert = {
