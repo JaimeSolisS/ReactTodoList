64b9565afae34e8b4bb45a2fea88589af2dc1f15
JaimeSolisS <jaimesolissoto@me.com>
2021-05-04 01:37:15 -0600

    feat add pwa assertion

--- client/lighthouserc-ci.js
+++ client/lighthouserc-ci.js
@@ -1,28 +1,33 @@
 module.exports = {
-    ci: {
-        collect: {
-            settings: {
-                emulatedFormFactor: 'desktop',
-                throttling: { rttMs: 40, throughputKbps: 10 * 1024, cpuSlowdownMultiplier: 1 },
-            },
-            startServerCommand: 'npm start',
-            numberOfRuns: 3,
-            url: [
-                'http://localhost:3000',
-                'http://localhost:3000/register',
-                'http://localhost:3000/projects',
-            ],
+  ci: {
+    collect: {
+      settings: {
+        emulatedFormFactor: "desktop",
+        throttling: {
+          rttMs: 40,
+          throughputKbps: 10 * 1024,
+          cpuSlowdownMultiplier: 1,
         },
-        assert: {
-            assertions: {
-                'categories:performance': ['error', { minScore: .9 }],
-                'categories:accessibility': ['error', { minScore: .9 }],
-                'categories:best-practices': ['error', { minScore: .9 }],
-                'categories:seo': ['error', { minScore: .9 }],
-            }
-        },
-        upload: {
-            target: "temporary-public-storage",
-        },
+      },
+      startServerCommand: "npm start",
+      numberOfRuns: 3,
+      url: [
+        "http://localhost:3000",
+        "http://localhost:3000/register",
+        "http://localhost:3000/projects",
+      ],
     },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:best-practices": ["error", { minScore: 0.9 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
+        "categories:pwa": ["error", { minScore: 0.9 }],
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
 };
