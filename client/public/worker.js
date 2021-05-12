83fb74f86688f3fcbe83747bc18242de246568f3
JaimeSolisS <jaimesolissoto@me.com>
2021-05-12 14:21:31 -0600

    fix error

--- client/public/worker.js
+++ client/public/worker.js
@@ -27,7 +27,7 @@ self.addEventListener("fetch", (event) => {
 
 // Update a service worker
 self.addEventListener("activate", (event) => {
-  var cacheWhitelist = ["pwa-task-manager"];
+  var cacheWhitelist = ["Todo-app"];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
